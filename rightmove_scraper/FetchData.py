d6e167901b3ccaef785558e706857cd174005522
joeanton719 <joeanton719@gmail.com>
2023-05-05 01:22:44 +0300

    new changes

--- rightmove_scraper/FetchData.py
+++ rightmove_scraper/FetchData.py
@@ -21,7 +21,6 @@ from utils.add_utils import Logger, fetch
 logger = Logger('rm_fetchData_log', 'rm_fetchData.log', currentdir).get_logger()
 
 
-
 async def fetch_all_property_data(outcode_list: list[str]) -> list[dict[str, Union[int, float, str, bool, None]]]:
     """
     Fetches property data for a list of outcodes using async requests.
@@ -50,7 +49,6 @@ async def fetch_all_property_data(outcode_list: list[str]) -> list[dict[str, Uni
         property_data = [data for data_list in property_data_list if len(data_list)!=0 for data in data_list]
         
         return property_data
-
 
 
 async def get_property_data(
@@ -108,11 +106,9 @@ async def get_property_data(
 
         except asyncio.exceptions.TimeoutError:
             logger.error("TimeoutError", search_url)
-            break
 
         except tenacity.RetryError:
             logger.error("RetryError", search_url)
-            break
 
     # return list of extracted property data
     return property_list
